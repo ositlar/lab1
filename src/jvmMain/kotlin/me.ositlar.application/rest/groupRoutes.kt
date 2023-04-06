186fe8822b6bd157fe01d22b79c53fd530d259d8
Максим <maxprometheus3@gmail.com>
2023-04-06 22:30:17 +0600

    lab2_fac_queries
    Signed-off-by: Максим <maxprometheus3@gmail.com>

--- src/jvmMain/kotlin/me.ositlar.application/rest/groupRoutes.kt
+++ src/jvmMain/kotlin/me.ositlar.application/rest/groupRoutes.kt
@@ -5,6 +5,7 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import me.ositlar.application.common.Item
 import me.ositlar.application.config.Config
 import me.ositlar.application.data.Student
 import me.ositlar.application.repo.studentsRepo
@@ -19,6 +20,30 @@ fun Route.groupRoutes() {
             } else {
                 call.respond(students.map { it.elem.group }.toSet())
             }
+        }
+        get("{id") {
+            val id =
+                call.parameters["id"] ?: return@get call.respondText(
+                "Missing or malformed id",
+                status = HttpStatusCode.BadRequest
+            )
+            val studentItem =
+                studentsRepo.read(id) ?: return@get call.respondText(
+                    "No groups with id $id",
+                    status = HttpStatusCode.NotFound
+                )
+            call.respond(studentItem)
+        }
+        put("{id}") {
+            val group = call.receive<String>()
+            val id = call.parameters["id"]!!
+            val student = studentsRepo.read(id)
+            val newStudent = Student(student!!.elem.firstname, student.elem.surname, group)
+            studentsRepo.update(id, newStudent)
+            call.respondText(
+                "Student updates correctly",
+                status = HttpStatusCode.Created
+            )
         }
     }
 }
